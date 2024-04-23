212f26476f9bdae98899ded9f9324d47aaab7cb9
WariHue <88533355+WariHue@users.noreply.github.com>
2024-04-23 18:17:33 +0900

    かわいい蜂

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/app"
 	"fyne.io/fyne/v2/container"
 	"fyne.io/fyne/v2/widget"
@@ -9,11 +10,15 @@ import (
 func main() {
 	a := app.New()
 	w := a.NewWindow("Hello")
+	w.Resize(fyne.NewSize(1680, 960))
 	hello := widget.NewLabel("Hello Fyne!")
 	w.SetContent(container.NewVBox(
 		hello,
 		widget.NewButton("Hi!", func() {
 			hello.SetText("Welcome :)")
+		}),
+		widget.NewButton("quit", func() {
+			a.Quit()
 		}),
 	))
 
